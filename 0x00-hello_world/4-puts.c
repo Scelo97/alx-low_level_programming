348fc4735b06f2b2ee2c876223cffea81031771a
Scelo97 <celescelo98@gmail.com>
2023-06-15 08:53:01 -0700

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,9 +1,12 @@
 #include <stdio.h>
 /**
  * main - Entry
- * Return: Always 0
+ *
+ * Return: Always 0 (success)
  */
 int main (void)
-{      puts ("/"Programming is like building a multilingual puzzle";
+{      
+	puts ("/"Programming is like building a multilingual puzzle";
 		return (0);
+		}		
 
