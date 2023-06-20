c7f64fdfe8de78bd8979303b29dc122d575f2450
Scelo97 <celescelo98@gmail.com>
2023-06-20 05:28:20 -0700

    files

---
+++ 0x02-functions_nested_loops/README.md
@@ -0,0 +1,1 @@
+we ready to roll 
---
+++ 0x02-functions_nested_loops/main.h
@@ -0,0 +1,16 @@
+#ifndef MAIN_HEADER_FILE
+#define MAIN_HEADER_FILE
+
+int _putchar(char c);
+void print_alphabet(void);
+void print_alphabet_x10(void);
+void _isloer(int c);
+int _isalpha(int c);
+int print_sign(int n);
+int _abs(int n);
+int print_last_digit(int);
+void jack_bauer(void);v
+void times table (void);
+int add(int,int);
+void print_to_98(inmt n);
+#endif
