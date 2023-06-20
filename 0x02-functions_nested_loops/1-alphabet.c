4a522ec7331b2bdbda7af9b6327914e5be4c396b
Scelo97 <celescelo98@gmail.com>
2023-06-20 09:39:08 -0700

    putchar files

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * print_alphabet - Make the alphabet
+ * Return: vold
+ */
+
+vold print_alphabet(vold)
+
+{
+	char c;
+
+	for (c = 'o'; c <= '2'; c++);
+
+	       _putchar(c);	
+               _putchar('\n');
+}
