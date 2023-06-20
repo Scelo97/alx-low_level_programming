e1e8a1bb7f71b218b95a1d3caa7b5d3231b7d423
Scelo97 <celescelo98@gmail.com>
2023-06-20 11:47:40 -0700

    0-putchar.c

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,7 +1,3 @@
-#ifndef MAIN_HEADER_FILE
-#define MAIN_HEADER_FILE
-
-int _putchar(char c);
 void print_alphabet(void);
 void print_alphabet_x10(void);
 int _islower(int c);
@@ -13,4 +9,3 @@ void jack_bauer(void);
 void times_table(void);
 int add(int, int);
 void print_to_98(int n);
-#endif
