5672691bab7a959f360be88999deef634b98bc5a
Scelo97 <celescelo98@gmail.com>
2023-06-26 09:40:47 -0700

    0-reset_to_98.c

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,12 +1,10 @@
 #include "main.h"
-
 /**
- * reset_to_98 - A function that takes a pointer to an int as parameter
+ * reset_to_98 - a function that takes a pointer to an int as parameter
  * and updates the valvue it points to 98
  * @n: input
  * Return: n
  */
-
 void reset_to_98(int *n)
 {
 	*n = 98;
