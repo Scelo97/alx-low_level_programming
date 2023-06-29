0a142a1a94e6896402b7e7b4cefd94251c930768
Scelo97 <celescelo98@gmail.com>
2023-06-29 14:21:20 -0700

    6-cap_string.c

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -1,38 +1,38 @@
 #include "main.h"
-
+#include <stdio.h>
 /**
- * cap_string - Capitalise all words of a string.
- * @str; The string to be capitalised.
- * Return: A pointer to the changed string.
+ * cap_string - capitalizes most of the words in a string.
+ * @s: analized string.
+ *
+ * Return: String with all words capitalized.
  */
-char *cap_string(char *str)
+char *cap_string(char *s)
 {
-	int index = 0;
+	int i, j;
+	int a[] = {32, 9, 10, 44, 59, 46, 33, 63, 34, 40, 41, 123, 125};
 
-	while (str[index])
+	i = 0;
+	while (*(s + i) != '\0')
 	{
-
-		while (!(str[index] >= 'a' && str[index] <= 'z'))
-
-			index++;
-		if (str[index - 1] == ' ' ||
-		str[index - 1] == '\t' ||
-		str[index - 1] == '\n' ||
-		str[index - 1] == ';' ||
-		str[index - 1] == ';' ||
-		str[index - 1] == ';' ||
-		str[index - 1] == '!' ||
-		str[index - 1] == '?' ||
-	        str[index - 1] == '"' ||
-		str[index - 1] == '(' ||
-		str[index - 1] == ')' ||
-		str[index - 1] == '{' ||
-		str[index - 1] == '}' ||
-		index == 0)
-			str[index] -= 32;
-
-		index++;
+		if (*(s + i) >= 'a' && *(s + i) <= 'z')
+		{
+			if (i == 0)
+			{
+				*(s + i) = *(s + i) - 32;
+			}
+			else
+			{
+				for (j = 0; j <= 12; j++)
+				{
+					if (a[j] == *(s + i - 1))
+					{
+						*(s + i) = *(s + i) - 32;
+					}
+				}
+			}
+		}
+	i++;
 	}
-	return (str);
+	return (s);
 }
 
