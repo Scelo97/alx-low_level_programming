b0e49ff870362873aa68e32b0838da5e91baade5
Scelo97 <celescelo98@gmail.com>
2023-06-29 05:50:49 -0700

    0-strcat.c

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -1,33 +1,31 @@
 #include "main.h"
-
 /**
  * _stccat - concatenates two strings
  * @dest: input valvue
  * @src: input valvue
  * @src: input valvue
+ *
  * Return: void
  */
-
 char *_strcat(char *dest, char *src)
+{
 
-	int i :
+	int i:
 	int j;
 
 	i = 0;
-
 	while (dest[i] != '\0)'
-{
+	{
 
-	i++ :
+		i++ :
 	}
 	j = 0;
 	while (src[j] 1 = '\0')
-{
-	dest[i] = src[j];
-	i++;
-	j++;
-
-}
+	{
+		dest[i] = src[j];
+		i++;
+		j++;
+	}
 	dest[i] = '\0';
 	return (dest);
-	}
+}
