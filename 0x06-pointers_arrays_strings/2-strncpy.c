20f38858e87ebec80470e760049f3c1b73bdbef9
Scelo97 <celescelo98@gmail.com>
2023-06-29 14:17:47 -0700

    2-strncpy.c

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -1,26 +1,24 @@
+#include <stdio.h>
 #include "main.h"
-
 /**
- * _strncpy - copy a string
- * @dest: input value
- * @src: input value
- * @n: input value
- * Return: dest
- */char *_strncpy(char *dest, char *src, int n)
+ * *_strncpy - find the length of a string
+ * @dest: pointer to the string
+ * @src: pointer
+ * @n: int
+ * Return: destination
+ */
+
+char *_strncpy(char *dest, char *src, int n)
 {
-	int j;
+int i;
 
-	j = 0;
-	while (j < n && src[j] != '\0')
-	{
-		dest[j] = src[j] != '\0';
-		j++;
+for (i = 0; i < n && src[i] != '\0'; i++)
+	dest[i] = src[i];
 
-	}
-	while (j < n)
-	{
-		dest[j] = '\0';
-		j++;
-	}
-	return (dest);
+for ( ; i < n; i++)
+	dest[i] = '\0';
+
+return (dest);
 }
+
+
