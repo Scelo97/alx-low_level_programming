b3714d99713a29ed4490fc84a623492f6410a019
Scelo97 <celescelo98@gmail.com>
2023-06-29 06:02:45 -0700

    0-strcat.c

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -10,17 +10,17 @@
 char *_strcat(char *dest, char *src)
 {
 
-	int i:
+	int i;
 	int j;
 
 	i = 0;
-	while (dest[i] != '\0)'
+	while (dest[i] != '\0')
 	{
 
-		i++ :
+		i++;
 	}
 	j = 0;
-	while (src[j] 1 = '\0')
+	while (src[j] != '\0')
 	{
 		dest[i] = src[j];
 		i++;
