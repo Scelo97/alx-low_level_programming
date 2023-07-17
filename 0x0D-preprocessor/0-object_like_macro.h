27abbcb6099388a0b4d13c31b7bb695c42459378
Scelo97 <celescelo98@gmail.com>
2023-07-17 10:07:11 -0700

    2-main.c

--- 0x0D-preprocessor/0-object_like_macro.h
+++ 0x0D-preprocessor/0-object_like_macro.h
@@ -1,24 +1,7 @@
-#include "main.h"
-#include <stdlib.h>
-/**
- * create_array - create array of size size and assign char c
- * @size: size of array
- * @c: char to assign
- * Description: create array of size size and assign char c
- * Return: pointer to array, NULL if fail
- *
- */
-char *create_array(unsigned int size, char c)
-{
-	char *str;
-	unsigned int i;
+#ifndef OBJECT_LIKE_MACRO_H
+#define OBJECT_LIKE_MACRO_H
 
-	str = malloc(sizeof(char) * size);
-	if (size == 0 || str == NULL)
-		return (NULL);
+#define SIZE 1024
 
-	for (i = 0; i < size; i++)
-		str[i] = c;
-	return (str);
-}
+#endif
 
